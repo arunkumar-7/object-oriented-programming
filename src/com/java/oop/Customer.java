86e9e6a47321cfd3bd7d6a59c2de09c2ca5fb3ab
arunkumar-7 <arunkumarbathini4@gmail.com>
2026-04-15 17:02:46 +0530

    feat: add customer class and update Main with sample data

---
+++ src/com/java/oop/Customer.java
@@ -0,0 +1,18 @@
+package com.java.oop;
+
+public class Customer {
+    int id;
+    String name;
+    String email;
+    long phoneNumber;
+    String address;
+    String city;
+    String state;
+    int pinCode;
+    boolean isPrimeMember;
+    double walletBalance;
+    int totalOrders;
+
+
+
+}
--- src/com/java/oop/Main.java
+++ src/com/java/oop/Main.java
@@ -3,6 +3,9 @@ package com.java.oop;
 public class Main {
     static void main() {
         Product product1 = new Product();
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("Product 1 Details");
+        System.out.println("---------------------------------------------------------------------------------");
         product1.id = 1111;
         product1.name = "dell inspiron 15 3530";
         product1.maxRetailPrice = 66000;
@@ -23,6 +26,9 @@ public class Main {
         System.out.println("Category:" + product1.category);
 
         Product product2 = new Product();
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("Product 2 Details");
+        System.out.println("---------------------------------------------------------------------------------");
         product2.id = 2222;
         product2.name = "Google Pixel 9a";
         product2.maxRetailPrice = 49999;
@@ -41,6 +47,33 @@ public class Main {
         System.out.println("is Stock Available:" + product2.isAvailable);
         System.out.println("Company:" + product2.company);
         System.out.println("Category:" + product2.category);
+
+        Customer customer = new Customer();
+        System.out.println("----------------------------------------------------------------------------------");
+        System.out.println("Customer Details");
+        System.out.println("----------------------------------------------------------------------------------");
+        customer.id = 11111;
+        customer.name = "Arun kumar";
+        customer.email = "arunkumar@gmail.com";
+        customer.phoneNumber = 9390590008L;
+        customer.address = "Warasiguda";
+        customer.city = "Secunderabad";
+        customer.state = "Telangana";
+        customer.pinCode = 506143;
+        customer.isPrimeMember = true;
+        customer.walletBalance = 7520.55D;
+        customer.totalOrders = 6;
+        System.out.println("Customer id:" + customer.id);
+        System.out.println("Customer Name:" + customer.name);
+        System.out.println("Customer Email:" + customer.email);
+        System.out.println("Customer Mobile Number:" + customer.phoneNumber);
+        System.out.println("Customer Address:" + customer.address);
+        System.out.println("Customer city:" + customer.city);
+        System.out.println("Customer state:" + customer.state);
+        System.out.println("customer pincode:" + customer.pinCode);
+        System.out.println("Is a Prime Member:" + customer.isPrimeMember);
+        System.out.println("Wallet Balance:" + customer.walletBalance);
+        System.out.println("Total orders:" + customer.totalOrders);
 
 
     }
