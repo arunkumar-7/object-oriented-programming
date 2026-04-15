8a02725bbe072bb5c80e8d175f90920b7153f6be
arunkumar-7 <arunkumarbathini4@gmail.com>
2026-04-15 16:17:34 +0530

    feat: add sample data for second product

--- src/com/java/oop/Main.java
+++ src/com/java/oop/Main.java
@@ -2,27 +2,45 @@ package com.java.oop;
 
 public class Main {
     static void main() {
-        Product product = new Product();
-        product.id = 11;
-        product.name = "dell inspiron 15 3530";
-        product.maxRetailPrice = 66000;
-        product.rating = 4.1F;
-        product.reviewsCount = 1093;
-        product.discountPercentage = 17;
-        product.isAvailable = true;
-        product.company = "Dell";
-        product.category = "Laptop";
+        Product product1 = new Product();
+        product1.id = 1111;
+        product1.name = "dell inspiron 15 3530";
+        product1.maxRetailPrice = 66000;
+        product1.rating = 4.1F;
+        product1.reviewsCount = 1093;
+        product1.discountPercentage = 17;
+        product1.isAvailable = true;
+        product1.company = "Dell";
+        product1.category = "Laptop";
+        System.out.println("id:" + product1.id);
+        System.out.println("Name:" + product1.name);
+        System.out.println("MRP:" + product1.maxRetailPrice);
+        System.out.println("Rating:" + product1.rating);
+        System.out.println("reviews:" + product1.reviewsCount);
+        System.out.println("Discount:" + product1.discountPercentage + "%");
+        System.out.println("is Stock Available:" + product1.isAvailable);
+        System.out.println("Company:" + product1.company);
+        System.out.println("Category:" + product1.category);
 
-
-        System.out.println("id:" + product.id);
-        System.out.println("Name:" + product.name);
-        System.out.println("MRP:" + product.maxRetailPrice);
-        System.out.println("Rating:" + product.rating);
-        System.out.println("reviews:" + product.reviewsCount);
-        System.out.println("Discount:" + product.discountPercentage + "%");
-        System.out.println("is Stock Available:" + product.isAvailable);
-        System.out.println("Company:" + product.company);
-        System.out.println("Category:" + product.category);
+        Product product2 = new Product();
+        product2.id = 2222;
+        product2.name = "Google Pixel 9a";
+        product2.maxRetailPrice = 49999;
+        product2.rating = 4.4F;
+        product2.reviewsCount = 248;
+        product2.discountPercentage = 14;
+        product2.isAvailable = true;
+        product2.company = "Google";
+        product2.category = "Mobile Phones";
+        System.out.println("id:" + product2.id);
+        System.out.println("Name:" + product2.name);
+        System.out.println("MRP:" + product2.maxRetailPrice);
+        System.out.println("Rating:" + product2.rating);
+        System.out.println("reviews:" + product2.reviewsCount);
+        System.out.println("Discount:" + product2.discountPercentage + "%");
+        System.out.println("is Stock Available:" + product2.isAvailable);
+        System.out.println("Company:" + product2.company);
+        System.out.println("Category:" + product2.category);
 
 
     }
